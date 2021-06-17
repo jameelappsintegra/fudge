c7095015c2f53cb6e52b0a30f72ceb159f6a076f
jameelappsint <mail4jameelmca@gmail.com>
2021-06-17 12:32:12 +0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'fudge';
-  isScroll: Boolean = false;
 
-  scrollContainer(){
-    console.log(this.isScroll)
-    this.isScroll = !this.isScroll;
-  }
-
-  onInit(){
-    this.scrollContainer();
-  }
+  onInit() {}
 }
